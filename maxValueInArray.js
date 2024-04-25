fd4b71f6057b2deaf70f124da8453389c3bddfaf
bread <chalida.bread@gmail.com>
2024-04-25 23:37:12 +0700

    feat:Add code to find the maximum value in array

---
+++ maxValueInArray.js
@@ -0,0 +1,7 @@
+const number = [6, 1, -50, 200, 7, 9, -13, 20];
+const maxValue = Math.max(...number);
+console.log(maxValue);
+
+//or
+const number1 = [6, 1, -50, 200, 7, 9, -13, 20];
+console.log(Math.max(...number1));
